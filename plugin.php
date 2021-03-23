a188f04eb161f25897a79fe5a9302891ddff2ac8
Sven <34579211+sutidor@users.noreply.github.com>
2021-03-23 15:24:20 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -11,21 +11,26 @@ Author URI: https://github.com/NothingTV
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 // Hook our custom function into the 'pre_redirect' event
-yourls_add_action( 'pre_redirect', 'warning_redirection' );
+yourls_add_action( 'pre_redirect', 'sz_track' );
 
 // Our custom function that will be triggered when the event occurs
-function warning_redirection( $args ) {
+function sz_track( $args ) {
     $url = $args[0];
-    $code = $args[1];
+	$code = $args[1];
 	
-	$matomo_url = ""; // Add your URL from matomo here. Example: analytics.example.de
-	$matomo_id = ""; // Add your Tracking ID here
+	$matomo_url = "matomo-url"; // Add your URL from matomo here. Example: analytics.example.de
+	$matomo_id = "matomo-install-id"; // Add your Tracking ID here
+
+	$keyword = str_replace('/','',$_SERVER['REQUEST_URI']);
+	$destination = yourls_get_keyword_longurl($keyword);
+
 	$content = "<html><head><meta http-equiv=\"refresh\" content=\"0; URL='$url'\">
 	<!-- Matomo -->
 	<script type=\"text/javascript\">
 	  var _paq = window._paq || [];
 	  /* tracker methods like \"setCustomDimension\" should be called before \"trackPageView\" */
-	  _paq.push(['trackPageView']);
+	  //paq.push(['trackPageView']);
+	  _paq.push(['trackEvent', 'YOURLS', '" . $keyword . "', '" . $destination . "']);
 	  _paq.push(['enableLinkTracking']);
 	  (function() {
 		var u=\"https://" . $matomo_url . "/\";
